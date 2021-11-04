3a076962e7fe3db57f88a842f298f74ced81b0b6
Conrad Ludgate <conradludgate@gmail.com>
2021-11-04 20:21:53 +0000

    more tests

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@ mod tests {
     use crate::{Context, floats::{FloatComp, var::Var}};
 
     #[test]
-    fn it_works() {
+    fn quadratic() {
         let x: Var = "x".into();
 
         let y = x * x + x * 2.0 + 1.0;
@@ -37,5 +37,25 @@ mod tests {
 
         assert_eq!(y.eval(&ctx), 25.0);
         assert_eq!(y_.eval(&ctx), 10.0);
+    }
+
+    #[test]
+    fn multi() {
+        let x: Var = "x".into();
+        let y: Var = "y".into();
+
+        let z = x * x * y;
+        let z_x = z.diff(x);
+        let z_y = z.diff(y);
+
+        println!("{:?}", z);
+        println!("{:?}", z_x);
+        println!("{:?}", z_y);
+
+        let ctx = Context::new([("x", 4.0), ("y", 3.0)].into_iter().collect());
+
+        assert_eq!(z.eval(&ctx), 48.0);
+        assert_eq!(z_x.eval(&ctx), 24.0);
+        assert_eq!(z_y.eval(&ctx), 16.0);
     }
 }
